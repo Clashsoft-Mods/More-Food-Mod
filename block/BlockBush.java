a367b1d8a90f10dbcaf3e0d6822f5731cacce895
Clashsoft <clash2@hotmail.de>
2013-09-07 23:53:12 +0200

    Fixed Bush pick

--- block/BlockBush.java
+++ block/BlockBush.java
@@ -12,16 +12,42 @@ import net.minecraft.util.Icon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+/**
+ * The Class BlockBush.
+ */
 public class BlockBush extends BlockFlower
 {
+	
+	/** The max meta. */
 	public int maxMeta = 3;
 	
+	/** The drop. */
 	public ItemStack drop;
+	
+	/** The bush texture. */
 	public String bushTexture;
+	
+	/** The stem texture. */
 	public String stemTexture;
+	
+	/** The bush icon. */
 	public Icon bushIcon;
+	
+	/** The stem icon. */
 	public Icon stemIcon;
 	
+	/**
+	 * Instantiates a new block bush.
+	 * 
+	 * @param par1
+	 *            the par1
+	 * @param drop
+	 *            the drop
+	 * @param bushtexture
+	 *            the bushtexture
+	 * @param stemtexture
+	 *            the stemtexture
+	 */
 	public BlockBush(int par1, ItemStack drop, String bushtexture, String stemtexture)
 	{
 		super(par1, Material.plants);
@@ -31,16 +57,22 @@ public class BlockBush extends BlockFlower
 		this.stemTexture = stemtexture;
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#setBlockBoundsBasedOnState(net.minecraft.world.IBlockAccess, int, int, int)
+	 */
 	@Override
 	public void setBlockBoundsBasedOnState(IBlockAccess par1World, int par2, int par3, int par4)
 	{
 		if (par1World.getBlockMetadata(par2, par3, par4) == maxMeta)
-			this.setBlockBounds(0F, 0F, 0F, 1F, 1F, 1F);
+			this.setBlockBounds(0.1F, 0F, 0.1F, 0.9F, 0.9F, 0.9F);
 		else
-			this.setBlockBounds(0.3F, 0F, 0.3F, 0.7F, 1F, 0.7F);
+			this.setBlockBounds(0.3F, 0F, 0.3F, 0.7F, 0.9F, 0.7F);
 		this.setBlockBoundsForItemRender();
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#getBlockHardness(net.minecraft.world.World, int, int, int)
+	 */
 	@Override
 	public float getBlockHardness(World par1World, int par2, int par3, int par4)
 	{
@@ -60,11 +92,23 @@ public class BlockBush extends BlockFlower
 			par1World.setBlockMetadataWithNotify(par2, par3, par4, i + 1, 2);
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#onBlockDestroyedByPlayer(net.minecraft.world.World, int, int, int, int)
+	 */
 	@Override
 	public void onBlockDestroyedByPlayer(World par1World, int par2, int par3, int par4, int par5)
 	{
 		if (par5 == maxMeta)
 			par1World.setBlock(par2, par3, par4, blockID, 0, 2);
+	}
+	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.Block#idPicked(net.minecraft.world.World, int, int, int)
+	 */
+	@Override
+	public int idPicked(World par1World, int par2, int par3, int par4)
+	{
+		return drop.itemID;
 	}
 
 	/* (non-Javadoc)
@@ -107,6 +151,9 @@ public class BlockBush extends BlockFlower
 		this.blockIcon = this.stemIcon = par1IconRegister.registerIcon(stemTexture);
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.BlockFlower#getRenderType()
+	 */
 	@Override
 	public int getRenderType()
 	{
@@ -122,12 +169,28 @@ public class BlockBush extends BlockFlower
 		return true;
 	}
 	
+	/* (non-Javadoc)
+	 * @see net.minecraft.block.BlockFlower#isOpaqueCube()
+	 */
 	@Override
 	public boolean isOpaqueCube()
 	{
 		return false;
 	}
 
+	/**
+	 * Fertilize.
+	 * 
+	 * @param world
+	 *            the world
+	 * @param x
+	 *            the x
+	 * @param y
+	 *            the y
+	 * @param z
+	 *            the z
+	 * @return true, if successful
+	 */
 	public boolean fertilize(World world, int x, int y, int z)
 	{
 		return world.setBlockMetadataWithNotify(x, y, z, maxMeta, 2);
