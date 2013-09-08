22f05881e1c84c0abc926f9e317a846ba14819b4
Clashsoft <clash2@hotmail.de>
2013-09-08 11:20:56 +0200

    Added french fries, white and cow chocolate

--- food/Food.java
+++ food/Food.java
@@ -23,6 +23,48 @@ public class Food implements IItemMetadataList
 	public static Food[]		foodTypes				= new Food[1024];
 	public static List<Food>	foodList				= new ArrayList<Food>();
 	
+	// Vanilla Food Items
+	
+		public static Food			apple					= new Food((ItemFood) Item.appleRed, 0, null);
+		public static Food			appleStomped			= new Food(36, "Stomped Apple", "apple_stomped", 4, new FoodRecipe(1, new ItemStack(Item.appleRed)));
+		public static Food			appleGold1				= new Food((ItemFood) Item.appleGold, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gag", "ggg", 'g', Item.ingotGold, 'a', Item.appleRed }));
+		public static Food			appleGold2				= new Food((ItemFood) Item.appleGold, 1, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gag", "ggg", 'g', Block.blockGold, 'a', Item.appleRed }));
+		public static Food			appleDiamond			= new Food(37, "Diamond Apple", "apple_diamond", 8, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.appleRed })).setEffects(new PotionEffect(Potion.jump.id, 600, 1), new PotionEffect(Potion.damageBoost.id, 1200, 2));
+		
+		public static Food			melon					= new Food((ItemFood) Item.melon, 0, null);
+		public static Food			melonGold1				= new Food(Item.speckledMelon, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gmg", "ggg", 'g', Item.goldNugget, 'm', Item.melon }));
+		
+		public static Food			potato					= new Food((ItemSeedFood) Item.potato, 0, null);
+		public static Food			potatoCooked			= new Food((ItemFood) Item.bakedPotato, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.potato), 0.1F));
+		public static Food			potatoStomped			= new Food(38, "Stomped Potato", "potato_stomped", 2, new FoodRecipe(1, new ItemStack(Item.potato)));
+		public static Food			potatoGold1				= new Food(39, EnumChatFormatting.AQUA + "Golden Potato", "potato_gold", 5, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gpg", "ggg", 'g', Item.ingotGold, 'p', Item.potato })).setEffects(new PotionEffect(Potion.field_76444_x.id, 2400, 0));
+		public static Food			potatoGold2				= new Food(40, EnumChatFormatting.LIGHT_PURPLE + "Golden Potato", "potato_gold", 7, new FoodRecipe(CRAFTING, 1, new Object[] { "GGG", "GpG", "GGG", 'G', Block.blockGold, 'p', Item.potato })).setEffects(new PotionEffect(Potion.field_76444_x.id, 2400, 0), new PotionEffect(Potion.regeneration.id, 600, 4), new PotionEffect(Potion.fireResistance.id, 6000, 0));
+		public static Food			potatoDiamond			= new Food(41, "Diamond Potato", "potato_diamond", 6, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.potato })).setEffects(new PotionEffect(Potion.digSpeed.id, 600, 3), new PotionEffect(Potion.moveSpeed.id, 3000, 1));
+		public static Food			poisonousPotato			= new Food((ItemFood) Item.poisonousPotato, 0, null);
+		
+		public static Food			carrot					= new Food((ItemSeedFood) Item.carrot, 0, null);
+		public static Food			carrotCooked			= new Food(42, "Cooked Carrot", "carrot_cooked", 4, new FoodRecipe(FURNACE, 1, new ItemStack(Item.carrot), 0.1F));
+		public static Food			carrotStomped			= new Food(43, "Stomped Carrot", "carrot_stomped", 2, new FoodRecipe(1, new ItemStack(Item.carrot)));
+		public static Food			carrotGold1				= new Food((ItemFood) Item.goldenCarrot, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gcg", "ggg", 'g', Item.goldNugget, 'c', Item.carrot }));
+		public static Food			carrotDiamond			= new Food(44, "Diamond Carrot", "carrot_diamond", 6, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.carrot })).setEffects(new PotionEffect(Potion.invisibility.id, 3000, 0));
+		
+		public static Food			bread					= new Food((ItemFood) Item.bread, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "www", 'w', Item.wheat }));
+		public static Food			cookie					= new Food((ItemFood) Item.cookie, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "wcw", 'w', Item.wheat, 'c', new ItemStack(Item.dyePowder, 1, 3) }));
+		
+		public static Food			porkRaw					= new Food((ItemFood) Item.porkRaw, 0, null);
+		public static Food			porkCooked				= new Food((ItemFood) Item.porkCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.porkRaw), 0.1F));
+		public static Food			beefRaw					= new Food((ItemFood) Item.beefRaw, 0, null);
+		public static Food			beefCooked				= new Food((ItemFood) Item.beefCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.beefRaw), 0.1F));
+		public static Food			chickenRaw				= new Food((ItemFood) Item.chickenRaw, 0, null);
+		public static Food			chickenCooked			= new Food((ItemFood) Item.chickenCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.chickenRaw), 0.1F));
+		public static Food			fishRaw					= new Food((ItemFood) Item.fishRaw, 0, null);
+		public static Food			fishCooked				= new Food((ItemFood) Item.fishCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.fishRaw), 0.1F));
+		
+		public static Food			rottenFlesh				= new Food((ItemFood) Item.rottenFlesh, 0, null);
+		public static Food			spiderEye				= new Food((ItemFood) Item.spiderEye, 0, null);
+		
+		public static Food			pumpkinPie				= new Food((ItemFood) Item.pumpkinPie, 0, new FoodRecipe(1, new ItemStack(Item.egg), new ItemStack(Item.sugar), new ItemStack(Block.pumpkin)));
+	
 	public static Food			salad					= new Food(0, "Salad", "salad", 2, MoreFoodMod.saladPlantID);
 	public static Food			cucumber				= new Food(1, "Cucumber", "cucumber", 2, MoreFoodMod.cucumberPlantID);
 	public static Food			rice					= new Food(2, "Rice", "rice", 2, MoreFoodMod.ricePlantID);
@@ -59,6 +101,9 @@ public class Food implements IItemMetadataList
 	public static Food			cereals1				= new Food(33, "Cereals", "cereals_1", 0, new FoodRecipe(4, new ItemStack(Item.wheat)));
 	public static Food			cereals2				= new Food(34, "Chocolate Cereals", "cereals_2", 0, new FoodRecipe(8, new ItemStack(Item.wheat), new ItemStack(Item.dyePowder, 1, 3)));
 	public static Food			honeydrop				= new Food(35, "Honey Drop", "honeydrop", 1, 0);
+	public static Food			chocolateWhite			= new Food(36, "White Chocolate Bar", "chocolatebar_white", 4, new FoodRecipe(4, butter.asStack(), new ItemStack(Item.bucketMilk), new ItemStack(Item.bucketMilk), new ItemStack(Item.sugar)));
+	public static Food			chocolateCow			= new Food(37, "Cow Chocolate Bar", "chocolatebar_cow", 4, new FoodRecipe(4, new ItemStack(Item.dyePowder, 1, 3), new ItemStack(Item.dyePowder, 1, 3), new ItemStack(Item.bucketMilk), butter.asStack(), new ItemStack(Item.sugar)));
+	public static Food			frenchfries				= new Food(38, "French Fries", "frenchfries", 5, new FoodRecipe(1, potatoCooked.asStack(), potatoCooked.asStack(), butter.asStack(), new ItemStack(Item.paper), new ItemStack(Item.dyePowder, 1, 1)));
 	
 	public static Food			orange					= new Food(45, "Orange", "orange", 3, MoreFoodMod.fruitSaplingsID).setMetadataPlaced(0);
 	public static Food			pear					= new Food(46, "Pear", "pear", 3, MoreFoodMod.fruitSaplingsID).setMetadataPlaced(1);
@@ -82,48 +127,6 @@ public class Food implements IItemMetadataList
 	public static Food			icecreamConeCS			= new Food(90, "Icecream", "icecream_cone_c_s", 4, new FoodRecipe(CRAFTING, 1, new Object[] { "c", "s", "C", 'c', icecreamChocolate.asStack(), 's', icecreamStrawberry.asStack(), 'C', icecreamCone.asStack() }));
 	public static Food			icecreamConeVS			= new Food(91, "Icecream", "icecream_cone_v_s", 4, new FoodRecipe(CRAFTING, 1, new Object[] { "v", "s", "C", 'v', icecreamVanilla.asStack(), 's', icecreamStrawberry.asStack(), 'C', icecreamCone.asStack() }));
 	public static Food			icecreamConeCVS			= new Food(92, "Icecream", "icecream_cone_c_v_s", 5, new FoodRecipe(CRAFTING, 1, new Object[] { "cvs", " C ", 'c', icecreamChocolate.asStack(), 'v', icecreamVanilla.asStack(), 's', icecreamStrawberry.asStack(), 'C', icecreamCone.asStack() }));
-	
-	// Vanilla Food Items
-	
-	public static Food			apple					= new Food((ItemFood) Item.appleRed, 0, null);
-	public static Food			appleStomped			= new Food(36, "Stomped Apple", "apple_stomped", 4, new FoodRecipe(1, new ItemStack(Item.appleRed)));
-	public static Food			appleGold1				= new Food((ItemFood) Item.appleGold, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gag", "ggg", 'g', Item.ingotGold, 'a', Item.appleRed }));
-	public static Food			appleGold2				= new Food((ItemFood) Item.appleGold, 1, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gag", "ggg", 'g', Block.blockGold, 'a', Item.appleRed }));
-	public static Food			appleDiamond			= new Food(37, "Diamond Apple", "apple_diamond", 8, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.appleRed })).setEffects(new PotionEffect(Potion.jump.id, 600, 1), new PotionEffect(Potion.damageBoost.id, 1200, 2));
-	
-	public static Food			melon					= new Food((ItemFood) Item.melon, 0, null);
-	public static Food			melonGold1				= new Food(Item.speckledMelon, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gmg", "ggg", 'g', Item.goldNugget, 'm', Item.melon }));
-	
-	public static Food			potato					= new Food((ItemSeedFood) Item.potato, 0, null);
-	public static Food			potatoCooked			= new Food((ItemFood) Item.bakedPotato, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.potato), 0.1F));
-	public static Food			potatoStomped			= new Food(38, "Stomped Potato", "potato_stomped", 2, new FoodRecipe(1, new ItemStack(Item.potato)));
-	public static Food			potatoGold1				= new Food(39, EnumChatFormatting.AQUA + "Golden Potato", "potato_gold", 5, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gpg", "ggg", 'g', Item.ingotGold, 'p', Item.potato })).setEffects(new PotionEffect(Potion.field_76444_x.id, 2400, 0));
-	public static Food			potatoGold2				= new Food(40, EnumChatFormatting.LIGHT_PURPLE + "Golden Potato", "potato_gold", 7, new FoodRecipe(CRAFTING, 1, new Object[] { "GGG", "GpG", "GGG", 'G', Block.blockGold, 'p', Item.potato })).setEffects(new PotionEffect(Potion.field_76444_x.id, 2400, 0), new PotionEffect(Potion.regeneration.id, 600, 4), new PotionEffect(Potion.fireResistance.id, 6000, 0));
-	public static Food			potatoDiamond			= new Food(41, "Diamond Potato", "potato_diamond", 6, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.potato })).setEffects(new PotionEffect(Potion.digSpeed.id, 600, 3), new PotionEffect(Potion.moveSpeed.id, 3000, 1));
-	public static Food			poisonousPotato			= new Food((ItemFood) Item.poisonousPotato, 0, null);
-	
-	public static Food			carrot					= new Food((ItemSeedFood) Item.carrot, 0, null);
-	public static Food			carrotCooked			= new Food(42, "Cooked Carrot", "carrot_cooked", 4, new FoodRecipe(FURNACE, 1, new ItemStack(Item.carrot), 0.1F));
-	public static Food			carrotStomped			= new Food(43, "Stomped Carrot", "carrot_stomped", 2, new FoodRecipe(1, new ItemStack(Item.carrot)));
-	public static Food			carrotGold1				= new Food((ItemFood) Item.goldenCarrot, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "ggg", "gcg", "ggg", 'g', Item.goldNugget, 'c', Item.carrot }));
-	public static Food			carrotDiamond			= new Food(44, "Diamond Carrot", "carrot_diamond", 6, new FoodRecipe(CRAFTING, 1, new Object[] { "ddd", "dxd", "ddd", 'd', Item.diamond, 'x', Item.carrot })).setEffects(new PotionEffect(Potion.invisibility.id, 3000, 0));
-	
-	public static Food			bread					= new Food((ItemFood) Item.bread, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "www", 'w', Item.wheat }));
-	public static Food			cookie					= new Food((ItemFood) Item.cookie, 0, new FoodRecipe(CRAFTING, 1, new Object[] { "wcw", 'w', Item.wheat, 'c', new ItemStack(Item.dyePowder, 1, 3) }));
-	
-	public static Food			porkRaw					= new Food((ItemFood) Item.porkRaw, 0, null);
-	public static Food			porkCooked				= new Food((ItemFood) Item.porkCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.porkRaw), 0.1F));
-	public static Food			beefRaw					= new Food((ItemFood) Item.beefRaw, 0, null);
-	public static Food			beefCooked				= new Food((ItemFood) Item.beefCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.beefRaw), 0.1F));
-	public static Food			chickenRaw				= new Food((ItemFood) Item.chickenRaw, 0, null);
-	public static Food			chickenCooked			= new Food((ItemFood) Item.chickenCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.chickenRaw), 0.1F));
-	public static Food			fishRaw					= new Food((ItemFood) Item.fishRaw, 0, null);
-	public static Food			fishCooked				= new Food((ItemFood) Item.fishCooked, 0, new FoodRecipe(FURNACE, 1, new ItemStack(Item.fishRaw), 0.1F));
-	
-	public static Food			rottenFlesh				= new Food((ItemFood) Item.rottenFlesh, 0, null);
-	public static Food			spiderEye				= new Food((ItemFood) Item.spiderEye, 0, null);
-	
-	public static Food			pumpkinPie				= new Food((ItemFood) Item.pumpkinPie, 0, new FoodRecipe(1, new ItemStack(Item.egg), new ItemStack(Item.sugar), new ItemStack(Block.pumpkin)));
 	
 	static
 	{
@@ -195,6 +198,7 @@ public class Food implements IItemMetadataList
 		meatball.register();
 		salami.register();
 		hamburger.register();
+		frenchfries.register();
 		pizza.register();
 		omelette.register();
 		pumpkinPie.register();
@@ -206,6 +210,8 @@ public class Food implements IItemMetadataList
 		candycane.register();
 		butter.register();
 		chocolate.register();
+		chocolateWhite.register();
+		chocolateCow.register();
 		cookie.register();
 		chocolateCookie.register();
 		cereals1.register();
@@ -295,7 +301,7 @@ public class Food implements IItemMetadataList
 		}
 		catch (NoSuchMethodError err)
 		{
-			s = ModLoader.<String, Item> getPrivateValue(Item.class, item, "field_111218_cA");
+			s = ModLoader.<String, Item> getPrivateValue(Item.class, item, "iconString");
 		}
 		return s;
 	}
